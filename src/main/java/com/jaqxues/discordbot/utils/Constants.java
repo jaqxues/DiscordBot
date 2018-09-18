44e5dc86f57162b80187a0f623808f6d100bf6f3
Jacques Hoffmann <32979131+jaqxues@users.noreply.github.com>
2018-09-18 18:59:19 +0200

    Minor Changes. End of challenge, time left: 5h

--- src/main/java/com/jaqxues/discordbot/utils/Constants.java
+++ src/main/java/com/jaqxues/discordbot/utils/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
     public static final String TOKEN_TXT_PATH = "D:\\Documents\\Jacques\\CodeProjects\\IdeaProjects\\DiscordBot\\Redacted\\Token.txt";
     public static final String IDS_PROVIDER_JSON = "D:\\Documents\\Jacques\\CodeProjects\\IdeaProjects\\DiscordBot\\Redacted\\IdsProvider.json";
     public static final String SESSION_MANAGER_JSON = "D:\\Documents\\Jacques\\CodeProjects\\IdeaProjects\\DiscordBot\\Redacted\\SessionManager.json";
-    public static final String LOGBACK_CONFIG_PATH = "D:\\Documents\\Jacques\\CodeProjects\\IdeaProjects\\DiscordBot\\src\\main\\resources\\chapters\\configuration\\logback.xml";
+    public static final String LOGBACK_CONFIG_PATH = "chapters/configuration/logback.xml";
     public static final String BOT_VERSION = "1.0.0.0b";
 
     // ModulePack Compiler Constants
